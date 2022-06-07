f82fc3a622df43575d883f29ce59825b6b2892f7
Chinu <lachinu1@gmail.com>
2022-06-07 00:42:42 -0300

    Updates

---
+++ precache-manifest.3718ada779ddc229ec64bd393ac841e2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3185dbf916f6848b24920bfc8ec35277",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "e4152c0ade95ad3d8358",
+    "url": "/porfolio/static/css/main.8d02bf2d.chunk.css"
+  },
+  {
+    "revision": "a48870f4dca847bfdf01",
+    "url": "/porfolio/static/js/2.844c59c5.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/porfolio/static/js/2.844c59c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4152c0ade95ad3d8358",
+    "url": "/porfolio/static/js/main.63584ca1.chunk.js"
+  },
+  {
+    "revision": "0a5a44b9d39ddbc4a4db",
+    "url": "/porfolio/static/js/runtime-main.e6904c30.js"
+  },
+  {
+    "revision": "50e94750acbb57f77f8a38b2e6ea3277",
+    "url": "/porfolio/static/media/bitwallet.50e94750.png"
+  },
+  {
+    "revision": "a4c04172a5acb8180d61d8736d0addf4",
+    "url": "/porfolio/static/media/bitwallet.a4c04172.png"
+  },
+  {
+    "revision": "67b92b6e76bb7f612f34e9f031f6d479",
+    "url": "/porfolio/static/media/cinema.67b92b6e.png"
+  },
+  {
+    "revision": "a70aa613dad237f7127d4d1cfd567f4e",
+    "url": "/porfolio/static/media/cinema.a70aa613.png"
+  },
+  {
+    "revision": "f3e53e96a49faf50457fc78f70446dde",
+    "url": "/porfolio/static/media/flo.f3e53e96.png"
+  },
+  {
+    "revision": "f390210768eac97468141673a3aa9d35",
+    "url": "/porfolio/static/media/gifos.f3902107.png"
+  },
+  {
+    "revision": "0e28bda1a190da4b6a4de0d34356f2aa",
+    "url": "/porfolio/static/media/hoteles.0e28bda1.png"
+  },
+  {
+    "revision": "34e576924cdb3acf588cb9050d19814c",
+    "url": "/porfolio/static/media/playbrands.34e57692.png"
+  },
+  {
+    "revision": "4c273546a316c811ceb38c2c74bb2ac1",
+    "url": "/porfolio/static/media/rewardsstore.4c273546.png"
+  },
+  {
+    "revision": "59524207b7b1e2f56a8e90acce1aef42",
+    "url": "/porfolio/static/media/sh.59524207.png"
+  },
+  {
+    "revision": "8eff0eaf2547f389cf4cf755a8158e72",
+    "url": "/porfolio/static/media/southernhorizon.8eff0eaf.png"
+  }
+]);
